dd39445e85286bff53d1cfb7a637fd623dd9c774
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-09 15:17:20 +0200

    4.2. Add props to DetailsTab

--- src/components/details-tab/details-tab.jsx
+++ src/components/details-tab/details-tab.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
+import filmType from '../../types/film.js';
 
 
 const DetailsTab = (props) => {
+  const {
+    details,
+    genre,
+    year,
+  } = props.film;
+
+  const {
+    actors,
+    director,
+    length,
+  } = details;
+
+  console.log(genre);
+
   return (
     <div className="movie-card__text movie-card__row">
       <div className="movie-card__text-col">
         <p className="movie-card__details-item">
-          <strong className="movie-card__details-name">Director</strong>
-          <span className="movie-card__details-value">Wes Andreson</span>
+          <strong className="movie-card__details-name">
+            Director
+          </strong>
+          <span className="movie-card__details-value">
+            {director}
+          </span>
         </p>
         <p className="movie-card__details-item">
-          <strong className="movie-card__details-name">Starring</strong>
+          <strong className="movie-card__details-name">
+            Starring
+          </strong>
           <span className="movie-card__details-value">
-            Bill Murray, <br/>
-            Edward Norton, <br/>
-            Jude Law, <br/>
-            Willem Dafoe, <br/>
-            Saoirse Ronan, <br/>
-            Tony Revoloru, <br/>
-            Tilda Swinton, <br/>
-            Tom Wilkinson, <br/>
-            Owen Wilkinson, <br/>
-            Adrien Brody, <br/>
-            Ralph Fiennes, <br/>
-            Jeff Goldblum
+            {actors.map((actor, i, allActors) => {
+              return <React.Fragment key={actor + i}>
+                {actor}{i + 1 !== allActors.length && `, `}<br/>
+              </React.Fragment>;
+            })}
           </span>
         </p>
       </div>
@@ -31,20 +45,31 @@ const DetailsTab = (props) => {
       <div className="movie-card__text-col">
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Run Time</strong>
-          <span className="movie-card__details-value">1h 39m</span>
+          <span className="movie-card__details-value">
+            {length.hours}h {length.minutes}m
+          </span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Genre</strong>
-          <span className="movie-card__details-value">Comedy</span>
+          <span className="movie-card__details-value">
+            {genre.map((it, i, genres) => {
+              return <React.Fragment key={it + i}>
+                {it}{i + 1 !== genres.length && `, `}<br/>
+              </React.Fragment>;
+            })}
+          </span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Released</strong>
-          <span className="movie-card__details-value">2014</span>
+          <span className="movie-card__details-value">{year}</span>
         </p>
       </div>
     </div>
   );
 };
+
+
+DetailsTab.propTypes = filmType;
 
 
 export default DetailsTab;
