920451771657facc3044e9fe2373f4417a9611b9
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-12 04:25:42 +0200

    5.1. Update Logo component

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 
-const Logo = () => {
+const Logo = (props) => {
+  const {
+    extraLinkClass,
+  } = props;
+
   return (
     <div className="logo">
-      <Link to="/" className="logo__link">
+      <Link to="/" className={`logo__link ${extraLinkClass || ``}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
       </Link>
     </div>
   );
+};
+
+
+Logo.propTypes = {
+  extraLinkClass: PropTypes.string,
 };
 
 
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import history from '../../history.js';
 import filmType from '../../types/film.js';
 import FilmList from '../film-list/film-list.jsx';
+import Logo from '../logo/logo.jsx';
 import Footer from '../footer/footer.jsx';
 
 
@@ -33,13 +34,7 @@ const Main = (props) => {
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header movie-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo/>
 
           <div className="user-block">
             <div className="user-block__avatar">
