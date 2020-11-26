a7b63b6e8f576b76a723b1bfd428a32b748ab443
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-27 01:03:39 +0200

    7.3. Refactor ReviewsTab to display comments in 2 columns

---
+++ src/components/comment/comment.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Comment = (props) => {
+  const {
+    date,
+    name,
+    rating,
+    text,
+  } = props;
+
+  return (
+    <div className="review">
+      <blockquote className="review__quote">
+        <p className="review__text">{text}</p>
+
+        <footer className="review__details">
+          <cite className="review__author">{name}</cite>
+          <time className="review__date" dateTime={date}>
+            {/* TODO: Форматировать дату, когда будут приходить реальные данные */}
+            {date}
+          </time>
+        </footer>
+      </blockquote>
+
+      <div className="review__rating">
+        {/* TODO: Нужно ли менять формат отображения? */}
+        {rating}
+      </div>
+    </div>
+  );
+};
+
+
+Comment.propTypes = {
+  date: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+
+export default Comment;
