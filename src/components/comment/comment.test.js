deeef570d1e4d6a22a298e1e00515951c4feb954
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-28 01:42:16 +0200

    8.1. Add Comment test

---
+++ src/components/comment/comment.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Comment from './comment.jsx';
+
+
+it(`Should render correctly`, () => {
+  const tree = renderer
+    .create(
+        <Comment
+          date="2020-11-03T13:38:44.769Z"
+          name="John Doe"
+          rating={8.5}
+          text="This is mock comment"
+        />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
