6aa3c14356db36498fa2dc2230edee096ba5adc5
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-28 21:21:10 +0200

    8.1. Add DetailsTab snapshot test

--- src/components/details-tab/details-tab.test.js
+++ src/components/details-tab/details-tab.test.js
@@ -1,42 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import DetailsTab from './details-tab.jsx';
+import filmsMock from '../../mocks/films.js';
 
 
-const filmMock = {
-  cover: ``,
-  poster: ``,
-  preview: ``,
-  name: ``,
-  genre: `Biography`,
-  year: 2004,
-  description: ``,
-  rating: 7.5,
-  votes: 274,
-  details: {
-    director: `Martin Scorsese`,
-    actors: [
-      `Leonardo DiCaprio`,
-      `Cate Blanchett`,
-      `Alec Baldwin`,
-      `Kate Beckinsale`,
-      `John C. Reilly`,
-      `Alan Alda`,
-      `Ian Holm`,
-    ],
-    length: {
-      hours: 2,
-      minutes: 50,
-    },
-  },
-};
-
-
-it(`Should render correctly`, () => {
+it(`Should DetailsTab render correctly`, () => {
   const tree = renderer
     .create(
         <DetailsTab
-          film={filmMock}
+          film={filmsMock[0]}
         />
     )
     .toJSON();
