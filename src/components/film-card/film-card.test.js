35ff9086d744e692f653ff01772c7cd4ab218dba
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-28 21:18:00 +0200

    8.1. Add FilmCard snapshot test

--- src/components/film-card/film-card.test.js
+++ src/components/film-card/film-card.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  from './';
+import FilmCard from './film-card.jsx';
+import filmsMock from '../../mocks/films.js';
 
+
+const noop = () => {};
 
 it(`Should render correctly`, () => {
   const tree = renderer
-    .create()
+    .create(
+        <FilmCard
+          film={filmsMock[0]}
+          isHovered={false}
+          onMouseEnter={noop}
+          onMouseLeave={noop}
+          renderVideo={noop}
+        />
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
