f7ec12e425ba60e385fd3d7d3db5100bf8a33cbb
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-28 23:22:56 +0200

    8.1. Add FilmNavItem snapshot test

--- src/components/film-nav-item/film-nav-item.test.js
+++ src/components/film-nav-item/film-nav-item.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  from './';
+import FilmNavItem from './film-nav-item.jsx';
+import {FilmTabName} from '../../const.js';
 
 
-it(`Should render correctly`, () => {
+const noop = () => {};
+
+it(`Should FilmNavItem render correctly`, () => {
   const tree = renderer
-    .create()
+    .create(
+        <FilmNavItem
+          isActive={true}
+          name={FilmTabName.OVERVIEW}
+          onClick={noop}
+        />
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
