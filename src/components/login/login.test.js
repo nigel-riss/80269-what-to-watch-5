2712c5e8d2cf19178b7a5ca71ea953f440aba1fa
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-29 00:10:16 +0200

    8.1. Add Login snapshot test

--- src/components/login/login.test.js
+++ src/components/login/login.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  from './';
+import {MemoryRouter} from 'react-router-dom';
+import {Login} from './login.jsx';
 
 
-it(`Should render correctly`, () => {
+const noop = () => {}
+
+it(`Should Login render correctly`, () => {
   const tree = renderer
-    .create()
+    .create(
+        <MemoryRouter>
+          <Login
+            onSubmit={noop}
+          />
+        </MemoryRouter>
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
