332cc917ffaf05f6ff16cd7f46a8b549e0963fee
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-28 22:51:34 +0200

    8.1. Add Film snapshot test

---
+++ src/components/app/app.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+import {MemoryRouter} from "react-router-dom";
+import configureMockStore from 'redux-mock-store';
+import App from './app.jsx';
+import storeMock from '../../mocks/store';
+
+
+const mockStore = configureMockStore([]);
+const store = mockStore(storeMock);
+
+
+it(`Should App render correctly`, () => {
+  const tree = renderer
+    .create(
+        <Provider store={store}>
+          <MemoryRouter>
+            <App/>
+          </MemoryRouter>
+        </Provider>
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
--- src/components/film/film.test.js
+++ src/components/film/film.test.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
 import {MemoryRouter} from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
 import {Film} from './film.jsx';
 import filmsMock from '../../mocks/films.js';
 import reviewsMock from '../../mocks/reviews.js';
+import storeMock from '../../mocks/store';
 import {
   AuthorizationStatus,
   FilmTabName,
 } from '../../const.js';
 
 
+const mockStore = configureMockStore([]);
+const store = mockStore(storeMock);
 const noop = () => {};
 
 
-it(`Should Film render correctly`, () => {/
+it(`Should Film render correctly`, () => {
   const tree = renderer
     .create(
-        <MemoryRouter>
-          <Film
-            activeTab={FilmTabName.OVERVIEW}
-            authorizationStatus={AuthorizationStatus.NO_AUTH}
-            currentFilm={filmsMock[0]}
-            filmId={1}
-            films={filmsMock}
-            getFilm={noop}
-            onTabClick={noop}
-            reviews={reviewsMock}
-          />
-        </MemoryRouter>
+        <Provider store={store}>
+          <MemoryRouter>
+            <Film
+              activeTab={FilmTabName.OVERVIEW}
+              authorizationStatus={AuthorizationStatus.NO_AUTH}
+              currentFilm={filmsMock[0]}
+              filmId={1}
+              films={filmsMock}
+              getFilm={noop}
+              onTabClick={noop}
+              reviews={reviewsMock}
+            />
+          </MemoryRouter>
+        </Provider>, {
+          createNodeMock: () => {
+            return {};
+          }
+        }
     )
     .toJSON();
 
