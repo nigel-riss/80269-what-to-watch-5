cbeedf6343b416d00527c9defa583bee1641912d
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-29 01:54:37 +0200

    8.1. Add OverviewTab snapshot test

--- src/components/overview-tab/overview-tab.test.js
+++ src/components/overview-tab/overview-tab.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  from './';
+import OverviewTab from './overview-tab.jsx';
+import filmsMock from '../../mocks/films.js';
 
 
-it(`Should render correctly`, () => {
+it(`Should OverviewTab render correctly`, () => {
   const tree = renderer
-    .create()
+    .create(
+        <OverviewTab
+          film={filmsMock[0]}
+        />
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
