1624b2378b74adeabfb547b6ff0c67834e5efcc0
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-29 01:29:26 +0200

    8.1. Add Main and Logo snapshot tests

--- src/components/logo/logo.test.js
+++ src/components/logo/logo.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  from './';
+import Logo from './logo.jsx';
+import {MemoryRouter} from 'react-router-dom';
 
 
-it(`Should render correctly`, () => {
+it(`Should Logo render correctly`, () => {
   const tree = renderer
-    .create()
+    .create(
+        <MemoryRouter>
+          <Logo
+            isLight={true}
+          />
+        </MemoryRouter>
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  from './';
+import {Provider} from 'react-redux';
+import {MemoryRouter} from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
+import {Main} from './main.jsx';
+import filmsMock from '../../mocks/films.js';
+import storeMock from '../../mocks/store';
+import {FILMS_SHOWN_PER_CLICK} from '../../const.js';
 
+const mockStore = configureMockStore([]);
+const store = mockStore(storeMock);
+const noop = () => {};
 
-it(`Should render correctly`, () => {
+it(`Should Main render correctly`, () => {
   const tree = renderer
-    .create()
+    .create(
+        <Provider store={store}>
+          <MemoryRouter>
+            <Main
+              activeGenre={`Action`}
+              films={filmsMock}
+              filmsByGenre={filmsMock.slice(0, 4)}
+              itemsShownCount={FILMS_SHOWN_PER_CLICK}
+              onGenreSelect={noop}
+              onMoreButtonClick={noop}
+              promoFilm={filmsMock[0]}
+            />
+          </MemoryRouter>
+        </Provider>, {
+          createNodeMock: () => {
+            return {};
+          }
+        }
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
