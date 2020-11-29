df086fdfeb31f0d011aa235b6690bbb1ed183a9b
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-29 03:00:15 +0200

    8.1. Add UserBlock snapshot test

--- src/components/user-block/user-block.test.js
+++ src/components/user-block/user-block.test.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  from './';
+import {MemoryRouter} from 'react-router-dom';
+import {UserBlock} from './user-block.jsx';
+import {AuthorizationStatus} from '../../const.js';
 
 
-it(`Should render correctly`, () => {
+const noop = () => {};
+
+it(`Should UserBlock render correctly`, () => {
   const tree = renderer
-    .create()
+    .create(
+        <MemoryRouter>
+          <UserBlock
+            authorizationStatus={AuthorizationStatus.AUTH}
+            onAvatarClick={noop}
+            userAvatarUrl={`/img/avatar.png`}
+          />
+        </MemoryRouter>
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
