255da925b0c2866a3407d9ccfbc00ca7ea458c0e
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-29 07:23:27 +0200

    8.1. Update snapshot tests

--- src/components/player/player.test.js
+++ src/components/player/player.test.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Player from './Player';
+import {Player} from './Player';
+import filmsMock from '../../mocks/films.js';
 
+const ref = React.createRef();
+const noop = () => {};
 
 it(`Should Player render correctly`, () => {
   const tree = renderer
     .create(
-        <Player/>
+        <Player
+          currentFilm={filmsMock[0]}
+          filmId={1}
+          getFilm={noop}
+          isLoading={false}
+          isPlaying={true}
+          onFullScreenClick={noop}
+          onPlayPauseClick={noop}
+          progress={33}
+          progressRef={ref}
+          videoRef={ref}
+          timeElapsed={123}
+        />
     )
     .toJSON();
 
