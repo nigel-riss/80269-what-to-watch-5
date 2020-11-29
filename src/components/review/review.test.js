96cfab1b011aa97e2def98107732db11a7a6545c
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-29 02:43:45 +0200

    8.1. Add Review snapshot test

--- src/components/review/review.test.js
+++ src/components/review/review.test.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  from './';
+import {Provider} from 'react-redux';
+import {MemoryRouter} from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
+import {Review} from './review.jsx';
+import filmsMock from '../../mocks/films.js';
+import storeMock from '../../mocks/store';
 
 
-it(`Should render correctly`, () => {
+const mockStore = configureMockStore([]);
+const store = mockStore(storeMock);
+const noop = () => {};
+
+it(`Should Review render correctly`, () => {
   const tree = renderer
-    .create()
+    .create(
+        <Provider store={store}>
+          <MemoryRouter>
+            <Review
+              currentFilm={filmsMock[0]}
+              getFilm={noop}
+              filmId={1}
+              onFormSubmit={noop}
+              renderForm={noop}
+            />
+          </MemoryRouter>
+        </Provider>
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
